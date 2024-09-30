e4610a11012e6ae521802581f928abc3ad61cc15
CollignonB <67335568+CollignonB@users.noreply.github.com>
2024-09-30 23:47:21 +0200

    ajout de commentaires

--- get_deck_list.py
+++ get_deck_list.py
@@ -6,22 +6,24 @@ from urllib.request import urlopen
 import re
 
 list_of_event = pd.read_json("mtgtop8.json", orient="index")
-
+print(list_of_event)
 # poc sur la permière liste 
+
 page = urlopen(list_of_event.loc[0]['url'])
 html = page.read().decode("latin-1")
 soup = BeautifulSoup(html, "html.parser")
 
-list_of_deck = str(soup.find_all("div", attrs={"style" : "flex:1;"}))
+# contient la liste des div qui représentent une liste de deck
+list_of_decks = str(soup.find_all("div", attrs={"style" : "flex:1;"}))
 
 deck_name, player, deck_url = [], [], []
 
-for line in list_of_deck.split(","):
-    important_info = line.split("><")[1:3]
+for line in list_of_decks.split(","):
+    important_info = line.split("><")[1:3] # récupère les données importantes dans les divs
     
-    deck_name.append(re.search('">.*?<', str(important_info), re.IGNORECASE).group()[2:-1])
-    player.append(re.search('player=.*?"', str(important_info), re.IGNORECASE).group()[7:-1].replace("+", " "))
-    deck_url.append(f"https://www.mtgtop8.com/event{re.search('href.*?>', str(important_info), re.IGNORECASE).group()[6:-2]}")
+    deck_name.append(re.search('">.*?<', str(important_info), re.IGNORECASE).group()[2:-1]) # extrait le nom du deck
+    player.append(re.search('player=.*?"', str(important_info), re.IGNORECASE).group()[7:-1].replace("+", " ")) # extrait le nom du joueur
+    deck_url.append(f"https://www.mtgtop8.com/event{re.search('href.*?>', str(important_info), re.IGNORECASE).group()[6:-2]}") # extrait l'url' du deck
 
 deck_data = pd.DataFrame(columns=["Player", "Deck", "URL"])
 for i in range(0, len(deck_name)):
