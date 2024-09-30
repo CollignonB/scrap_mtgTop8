32b759deaacfb2936b5139c2316f4324dbaba077
CollignonB <67335568+CollignonB@users.noreply.github.com>
2024-09-30 22:01:56 +0200

    gestion de la date

--- get_event.py
+++ get_event.py
@@ -13,6 +13,9 @@ page = urlopen(base_url)
 html = page.read().decode("latin-1")
 soup = BeautifulSoup(html, "html.parser")
 
+file_path = "C:/Users/Utilisateur/Documents/python/web_scrp_mtg/mtgtop8.json"
+# récupération de la date de dernière création du json,                        retrait de l'heure, changement de format
+date_last_update = datetime.datetime.fromtimestamp(os.path.getctime(file_path)).date().strftime("%d/%m/%y")
 
 # passage par une regex car l'utilisation de del est moche
 # pattern de regex pour trouver les urls
@@ -51,18 +54,18 @@ for row in list_of_events.findAll('tr', {"class"  : "hover_tr"}):
 
     # récupération des balises utilent dans le code html
     important = str(row.findAll('td')).split(",")[1::2]
-
-    # recréer et stock les urls des evenements
-    event_url.append(f"{base_url}{re.search(re_url_event, str(important), re.IGNORECASE).group()[1:-1]}&switch=text") 
-
-    # recupération du nom de l'evenement
-    if format != "EDH":
-        event_name.append(re.search(re_name_event, str(important), re.IGNORECASE).group()[4:-3])
-    else:
-        event_name.append(re.search(re_name_event, str(important), re.IGNORECASE).group()[5:-3])
-
+    # print("29/09/24" >= re.search(re_date_event, str(important), re.IGNORECASE).group()[5:-2] )
     # récuperation de la date de l'évenement
-    event_date.append(re.search(re_date_event, str(important), re.IGNORECASE).group()[5:-2])
+    if re.search(re_date_event, str(important), re.IGNORECASE).group()[5:-2] >= "29/09/24":
+        event_date.append(re.search(re_date_event, str(important), re.IGNORECASE).group()[5:-2])
+
+        event_url.append(f"{base_url}{re.search(re_url_event, str(important), re.IGNORECASE).group()[1:-1]}&switch=text") 
+
+        # recupération du nom de l'evenement
+        if format != "EDH":
+            event_name.append(re.search(re_name_event, str(important), re.IGNORECASE).group()[4:-3])
+        else:
+            event_name.append(re.search(re_name_event, str(important), re.IGNORECASE).group()[5:-3])
 
 # création d'un dictionnaire pour créer le Dataframe
 dict = {'name' : event_name, 'url' : event_url, 'date' : event_date}
@@ -73,11 +76,14 @@ evenement = evenement.sort_values(by=['date'], ascending = False).drop_duplicate
 event_json = evenement.to_json(orient='index')
 parsed = json.loads(event_json)
 
+print(evenement)
 
-file_path = "C:/Users/Utilisateur/Documents/python/web_scrp_mtg/mtgtop8.json"
-date_last_update = str(datetime.datetime.fromtimestamp(os.path.getctime(file_path))) 
+
+
+
 # A FAIRE : faire la convetrsion de format de la date et faire le test pour récupe seulement les nouveaux event en focntion de la date
 
-with open(file_path, 'w') as file:
-        json.dump(parsed, file, indent=2)
-        file.close()
+
+# with open(file_path, 'w') as file:
+#         json.dump(parsed, file, indent=2)
+#         file.close()
