4928f41ed6e238f39b0249a66a10f43754cebd59
CollignonB <67335568+CollignonB@users.noreply.github.com>
2024-10-15 18:46:49 +0200

    gestion de tout les evenements

--- get_deck_list.py
+++ get_deck_list.py
@@ -4,37 +4,29 @@ import json
 import os
 from urllib.request import urlopen
 import re
+from os import walk
 
-list_of_event = pd.read_json("mtgtop8.json", orient="index")
-print(list_of_event)
-# poc sur la permière liste 
+# recupération de la liste des fichiers d'evenement 
+list_of_deck = next(walk(r"C:\Users\Utilisateur\Documents\python\web_scrp_mtg\events"), (None, None, []))[2]
 
-page = urlopen(list_of_event.loc[0]['url'])
+# ouverutre d'un fichier d'evenement
+deck_lists = pd.read_json(fr"C:\Users\Utilisateur\Documents\python\web_scrp_mtg\events\{list_of_deck[8]}", orient="index")
+
+# for index, row in deck_lists.iterrows():
+    # print(deck_lists['Deck'][ind], deck_lists['URL'][ind])
+
+page = urlopen(deck_lists.loc[3]['URL'])
 html = page.read().decode("latin-1")
 soup = BeautifulSoup(html, "html.parser")
 
-# contient la liste des div qui représentent une liste de deck
-list_of_decks = str(soup.find_all("div", attrs={"style" : "flex:1;"}))
+card_name, copy = [], []
 
-deck_name, player, deck_url = [], [], []
+list_of_cards = str(soup.find_all("div", attrs={"style" : "margin:3px;flex:1;"}))
 
-for line in list_of_decks.split(","):
-    important_info = line.split("><")[1:3] # récupère les données importantes dans les divs
-    
-    deck_name.append(re.search('">.*?<', str(important_info), re.IGNORECASE).group()[2:-1]) # extrait le nom du deck
-    player.append(re.search('player=.*?"', str(important_info), re.IGNORECASE).group()[7:-1].replace("+", " ")) # extrait le nom du joueur
-    deck_url.append(f"https://www.mtgtop8.com/event{re.search('href.*?>', str(important_info), re.IGNORECASE).group()[6:-2]}") # extrait l'url' du deck
+for line in list_of_cards.split("<div")[2:]:
+    pass
 
-deck_data = pd.DataFrame(columns=["Player", "Deck", "URL"])
-for i in range(0, len(deck_name)):
-    deck_data.loc[i] = [player[i], deck_name[i], deck_url[i]]
-
-file_path = f"C:/Users/Utilisateur/Documents/python/web_scrp_mtg/{list_of_event.loc[0]['name']}.json"
-
-event_json = deck_data.to_json(orient='index')
-parsed = json.loads(event_json)
-
-with open(file_path, 'w') as file:
-        json.dump(parsed, file, indent=2)
-        file.close()
-
+"""
+regex qui recupère tout les noms de carte et le nombre d'exemplaire
+;">.*?</
+"""
